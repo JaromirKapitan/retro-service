0bee26a4f30c65d846491a46660d605cf7a2f292
Jarda <jarda@csweb.sk>
2025-09-30 13:15:24 +0200

    system - pull

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -3,29 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Storage;
 
 class SystemController extends Controller
 {
     public function pull()
     {
-        session()->flash('warning', "Currently not working!!");
+        Storage::put('pull', 'Do pull');
+
+        session()->flash('success', "Pull request nastaveny.");
         return redirect()->back();
-
-//        if(isLocalhost()) {
-////            session()->flash('warning', "Localhost nie je mozne aktualizovat.");
-//            return redirect()->back();
-//        }
-
-        $result = Process::path(base_path())->run('bash deploy.sh', function (string $type, string $output){
-            echo $output."<br/>";
-        });
-
-        sleep(5);
-
-        dd('END'); // todo: TMP
-
-//        session()->flash('success', "System aktualizovany.");
-//        return redirect()->back();
     }
 }
