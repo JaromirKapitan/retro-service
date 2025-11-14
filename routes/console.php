fe66cdd3f165a8449e56a400d6317a10bc32483c
Jarda <jarda@csweb.sk>
2025-11-14 13:05:29 +0100

    disables media conversion - todo

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('queue:work --no-interaction')->everyMinute()->withoutOverlapping();
+//Schedule::command('queue:work --no-interaction')->everyMinute()->withoutOverlapping();
