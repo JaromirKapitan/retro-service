41d5b0643796be00292d8b816473a7fe5965d3c6
Jarda <jarda@csweb.sk>
2025-11-18 14:08:33 +0100

    admin - filter vozidiel

---
+++ app/Livewire/Admin/Vehicles/Index.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Livewire\Admin\Vehicles;
+
+use App\Enums\VehicleType;
+use App\Models\Vehicle;
+use Livewire\Component;
+
+class Index extends Component
+{
+    public $filterOptions = [];
+    public $filter = [
+        'type' => null,
+        'brand' => null,
+        'model' => null,
+        'year' => null,
+    ];
+
+    public function boot()
+    {
+        $this->filterOptions = [
+            'types' => VehicleType::cases(),
+            'brands' => Vehicle::query()
+                ->distinct('brand')
+                ->whereNotNull('brand')
+                ->published()
+                ->orderBy('brand')
+                ->pluck('brand')
+                ->toArray(),
+            'models' => Vehicle::query()
+                ->distinct('model')
+                ->whereNotNull('model')
+                ->published()
+                ->orderBy('model')
+                ->pluck('model')
+                ->toArray(),
+        ];
+    }
+
+    public function getList()
+    {
+        $query = Vehicle::query();
+
+        if (!empty($this->filter['type'])) {
+            $query->where('type', $this->filter['type']);
+        }
+        if (!empty($this->filter['brand'])) {
+            $query->where('brand', $this->filter['brand']);
+        }
+
+        if (!empty($this->filter['model'])) {
+            $query->where('model', $this->filter['model']);
+        }
+
+        if (!empty($this->filter['year'])) {
+            $year = (int)$this->filter['year'];
+            $query->where(function ($q) use ($year) {
+                $q->where('year_from', '<=', $year)
+                    ->where(function ($q2) use ($year) {
+                        $q2->whereNull('year_to')
+                            ->orWhere('year_to', '>=', $year);
+                    });
+            });
+        }
+
+        return $query->published()->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.vehicles.index');
+    }
+}
---
+++ resources/views/livewire/admin/vehicles/index.blade.php
@@ -0,0 +1,78 @@
+<div class="container-fluid">
+    <section class="row mb-3">
+        <div class="col-sm-3">
+            <div>
+                <label for="type" class="form-label">{{ __('Type') }}</label>
+
+                <select id="type" name="type" class="form-control" wire:model.live="filter.type">
+                    <option value="0">-- {{ __('vehicle type') }} --</option>
+
+                    @foreach($filterOptions['types'] as $type)
+                        <option value="{{ $type->value }}">{{ __($type->getTitle()) }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div>
+                <label for="brand" class="form-label">{{ __('Brand') }}</label>
+
+                <select id="brand" name="brand" class="form-control" wire:model.live="filter.brand">
+                    <option value="0">-- {{ __('vehicle brand') }} --</option>
+
+                    @foreach($filterOptions['brands'] as $brand)
+                        <option value="{{ $brand }}">{{ $brand }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <div>
+                <label for="model" class="form-label">{{ __('Model') }}</label>
+
+                <select id="model" name="model" class="form-control" wire:model.live="filter.model">
+                    <option value="0">-- {{ __('vehicle model') }} --</option>
+
+                    @foreach($filterOptions['models'] as $vehicleModel)
+                        <option value="{{ $vehicleModel }}">{{ $vehicleModel }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <label for="year" class="form-label">{{ __('Year') }}</label>
+            <input type="text" class="form-control" id="year" placeholder="1885" wire:model.live="filter.year">
+        </div>
+    </section>
+
+    <table class="table table-hover border-1">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>{{ __('admin.title') }}</th>
+            <th>{{ __('admin.status') }}</th>
+            <th></th>
+        </tr>
+        </thead>
+
+        <tbody>
+        @foreach($this->getList() as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->title }}</td>
+                <td>
+                    <span class=" w-75 badge rounded-pill content-status-{{ $item->status }}">
+                        {{ __('admin.'.$item->status) }}
+                    </span>
+                </td>
+                <td class="text-end">
+                    <x-entity.table-buttons entity="vehicles" :item="$item" :multilang="\App\Enums\Lang::isMultilang()" />
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
