0d913d4b0e37be3e2495e0bdf14509adb2466fc9
Jarda <jarda@csweb.sk>
2025-11-28 22:02:15 +0100

    fix DBSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
+use App\Models\SeoData;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Vehicle;
@@ -26,5 +27,21 @@ class DatabaseSeeder extends Seeder
 //        WebPage::factory(25)->create();
 //        Article::factory(25)->create();
         Vehicle::factory(25)->create();
+        // for each vehicle
+        Vehicle::all()->each(function ($vehicle) {
+            // check if exist seo data for vehicle (seo data seoble_type=App\Models\Vehicle)
+            $seo = SeoData::where('seoble_type', 'App\Models\Vehicle')->where('seoble_id', $vehicle->id)->first();
+            if (!$seo) {
+                // create seo data for vehicle
+                SeoData::create([
+                    'seoble_id' => $vehicle->id,
+                    'seoble_type' => 'App\Models\Vehicle',
+                    'lang' => $vehicle->lang,
+                    'slug' => strtolower($vehicle->brand . '-' . $vehicle->model . '-' . $vehicle->id),
+                    'keywords' => implode(', ', [$vehicle->brand, $vehicle->model, $vehicle->type]),
+                ]);
+            }
+        });
+
     }
 }
