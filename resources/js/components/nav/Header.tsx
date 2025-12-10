d32437e688479edb7d58a0849b6a253910488359
kragleh <kragleh@gmail.com>
2025-12-10 13:07:21 +0100

    Use Inertia Link Component

--- resources/js/components/nav/Header.tsx
+++ resources/js/components/nav/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import { ThemeSwitch } from '../ui/theme-switch'
+import { Link } from '@inertiajs/react'
 // import { useTranslations } from 'next-intl'
 // import LanguageSwitch from '../ui/language-switch'
 
@@ -11,13 +12,13 @@ const Header = () => {
   return (
     <>
       <header className='flex items-center justify-between p-2'>
-        <a href={'/'} className='px-2 text-2xl font-semibold'>Retro Servis</a>
+        <Link href={'/'} className='px-2 text-2xl font-semibold'>Retro Servis</Link>
         <div className='flex gap-2'>
           <Button variant='ghost' asChild>
-            <a href='/'>Domov</a>
+            <Link href='/'>Domov</Link>
           </Button>
           <Button variant='ghost' asChild>
-            <a href='/vozidla'>Vozidla</a>
+            <Link href='/vozidla'>Vozidla</Link>
           </Button>
           {/* <LanguageSwitch /> */}
           <ThemeSwitch />
