8e8981062749f0971744a9f318fa4e651a52e106
Jarda <jarda@csweb.sk>
2025-12-11 11:38:29 +0100

    code cleanup

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,8 @@ Route::middleware(['sync.lang'])->group(function () {
     });
 });
 
-Route::get('language/{locale}', function ($locale) {
-    app()->setLocale($locale);
-    session()->put('locale', $locale);
-    return redirect('/');
-})->name('language');
-
 Route::get('/login', function () {
     return redirect()->route('admin.login');
 })->name('login');
 
-require_once __DIR__ . '/temp.php';
 require_once __DIR__ . '/admin.php';
-
-// musi byt posledny !!!
-Route::get('/{slug}', [App\Http\Controllers\HomeController::class, 'show'])->name('show');
