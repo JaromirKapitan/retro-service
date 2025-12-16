5943f1a06e5c9f69571308799d6f68054f70e9a6
Jarda <jarda@csweb.sk>
2025-12-16 17:12:18 +0100

    fix html title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8" />
-        <title>Laravel, Inertia, and React</title>
+        <title>{{ config('app.name') }}</title>
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx'])
         @inertiaHead
